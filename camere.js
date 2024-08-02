fa2c00381d75477dfffcd4eef5c92e3d31b51ab6
ciao <ciaociao@gmail.com>
2024-08-02 16:53:23 +0200

    camere

---
+++ camere.js
@@ -0,0 +1,39 @@
+$("#camerainfo").hide()
+$("#camera2info").hide()
+$("#camera3info").hide()
+
+$(".clicca").click(function(){
+    $(".clicca").css(`color`,`gray`)
+    $("#cameree").hide()
+    $("#camerainfo").show()
+})
+
+$(".clicca2").click(function(){
+    $(".clicca2").css(`color`,`grey`)
+    $("#camerainfo").hide()
+    $("#cameree").show()
+})
+
+$(".clicca3").click(function(){
+    $(".clicca3").css(`color`,`grey`)
+    $("#cameree").hide()
+    $("#camera2info").show()
+})
+
+$(".clicca4").click(function(){
+    $(".clicca4").css(`color`,`grey`)
+    $("#camera2info").hide()
+    $("#cameree").show()
+})
+
+$(".clicca5").click(function(){
+    $(".clicca5").css(`color`,`grey`)
+    $("#camera3info").hide()
+    $("#cameree").show()
+})
+
+$(".clicca6").click(function(){
+    $(".clicca6").css(`color`,`grey`)
+    $("#cameree").hide()
+    $("#camera3info").show()
+})
