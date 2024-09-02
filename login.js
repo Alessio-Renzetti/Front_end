3cb9100dce84b544393b0b2578dc016b0ab84ef5
FedericaDannibale <federicadannibale5@gmail.com>
2024-09-02 16:25:40 +0200

    ho salvato credenziali archiviazione locale

--- login.js
+++ login.js
@@ -3,4 +3,6 @@ $(".clicca").click(function(){
     user=$('.user').val()
     pass=$('.pass').val()
     console.log(user,pass)
+    localStorage.setItem("username", user)
+    localStorage.setItem("password", pass)
 })
