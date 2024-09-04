e5ea676d387c7f8c16fde18b93ff9c8e5efe4453
Andra261202 <aaandramaria02@gmail.com>
2024-09-04 15:44:08 +0200

    PRENOTAZIONI

--- prenotaCamere.js
+++ prenotaCamere.js
@@ -4,16 +4,27 @@ $(".clicca").click(function(){
 
     console.log(arrivo,partenza)
 
-    $.post({
-        url: "http://192.168.1.237:8080/prenotazioni/verifica",
-        data: JSON.stringify({
-            "arrivo": arrivo,
-            "partenza": partenza
+    if(arrivo>partenza){
+        alert('ERROR: inserisci una data valida')
+    }
+    else(
+        
+        $.post({
+            url: "http://192.168.1.237:8080/prenotazioni/verifica",
+            data: JSON.stringify({
+                "arrivo": arrivo,
+                "partenza": partenza
+    
+            }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success :function(response){
+                console.log(response)
+            }
+        })
+    )
 
-        }),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-    });
-
+        
+   
 
 })
