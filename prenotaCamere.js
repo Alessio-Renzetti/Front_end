1f7a5d9c369e6294deb6ac123c0d4967584d14e7
Andra261202 <aaandramaria02@gmail.com>
2024-09-05 15:28:37 +0200

    avete sbagliato voi

--- prenotaCamere.js
+++ prenotaCamere.js
@@ -2,7 +2,8 @@ $(".clicca").click(function(){
     arrivo=$('#arrivo').val();
     partenza=$('#partenza').val();
 
-    console.log(arrivo,partenza)
+    // console.log(arrivo,partenza)
+    
 
     if(arrivo>partenza){
         alert('ERROR: inserisci una data valida')
@@ -12,14 +13,32 @@ $(".clicca").click(function(){
         $.post({
             url: "http://192.168.1.237:8080/prenotazioni/verifica",
             data: JSON.stringify({
-                "arrivo": arrivo,
-                "partenza": partenza
+                "data_inizio": arrivo,
+                "data_fine": partenza
     
             }),
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
             success :function(response){
+              $(".prenota").remove();
                 console.log(response)
+
+
+                let elemento=$("<div>").
+                addClass("disponibilita").
+                text("Ecco le camere disponibili");
+                $("body").append(elemento)
+
+                for(x of response){
+                    for(i in x){
+                        
+                        console.log(x)
+                        console.log(i.values())
+                    }
+
+                }
+                
+                
             }
         })
     )
